0074b2c0fc45a51193261941b4d92150131fb81e
inamartini <113420609+inamartini@users.noreply.github.com>
2022-09-27 19:35:03 +0200

    Create Oving1.java

---
+++ Oving1.java
@@ -0,0 +1,16 @@
+public class Oving1 {
+
+
+
+    public static void main(String[] args) {
+
+        double lengdeTommer = 10.0;
+
+        double lengdeCm = lengdeTommer * 2.54;
+
+        System.out.println("10 tommer er " + lengdeCm + " centimeter.");
+
+
+    }
+    
+}
